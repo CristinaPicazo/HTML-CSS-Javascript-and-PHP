47161e63c670f86499cc203e01863d944cd26206
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2020-06-04 23:48:54 +0100

    Add files via upload

---
+++ script/Personal_Portfolio_Webpage.js
@@ -0,0 +1,13 @@
+function show() {
+    var elemento = document.getElementById("aditional");
+    
+    if (elemento.className == "hide") {
+        elemento.className = "visible";
+        elemento = document.getElementById("link_1");
+        elemento.innerHTML = "Hide it";
+    } else {
+        elemento.className = "hide";
+        elemento = document.getElementById("link_1");
+        elemento.innerHTML = "Continue Reading";
+    }
+}
