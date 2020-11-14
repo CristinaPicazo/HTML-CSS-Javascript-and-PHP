6a9064dafdd6e85e4736dfde3e83ed0150528b63
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2020-11-14 20:52:02 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,17 +1,3 @@
-function show() {
-    var elemento = document.getElementById("aditional");
-    
-    if (elemento.className == "hide") {
-        elemento.className = "visible";
-        elemento = document.getElementById("link_1");
-        elemento.innerHTML = "Hide it";
-    } else {
-        elemento.className = "hide";
-        elemento = document.getElementById("link_1");
-        elemento.innerHTML = "Continue Reading";
-    }
-}
-
-$(document).ready(function() {
-    $("h2").addClass("animated bounce");
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
 });
