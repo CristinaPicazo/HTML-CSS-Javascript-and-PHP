2545bae36de17a7c146424f42e68988f5fbf89f3
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2021-01-09 21:20:46 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+    //About me card
     $('[data-toggle="tooltip"]').tooltip();
+
+    //flip
+    AOS.init({
+      duration: 650,
+      once: true
+    });
+
+    //Read more
+    $('#collapseToRead').on('click', function () {
+        var text=$('#collapseToRead').text();
+        if(text === "Read More"){
+          $(this).html('Hide');
+        } else{
+          $(this).text('Read More');
+       }
+      });
+
+    
 });
