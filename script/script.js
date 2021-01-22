f2e32831e5315a1cf6a510b83040a31ad3c61a34
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2021-01-22 21:18:41 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,32 +1,23 @@
-$(document).ready(function(){
-    //About me card
-    $('[data-toggle="tooltip"]').tooltip();
 
-    //flip
-    AOS.init({
-      duration: 1200,
-    });
+'use strict';
 
-    (function(){
-      'use strict';
-      
-      angular.module('portafollio',[])
-    .controller('readMore',readMore);
 
-    function readMore($scope){
-      $scope.readText = "Read More";
+// Read more about me
+var portafolio = angular.module('portafolio',[]);
 
-      $scope.read = function(){
-        if($scope.readText === "Read More"){
-          $scope.readText = "Hide";
-          return $scope.readText;
-        }
-        else{
-          $scope.readText = "Read More";
-          return $scope.readText;
-        }
-      }
+portafolio.controller('readMoreController',function readMoreController($scope){
+  $scope.readText = "Read More";
+
+  $scope.read = function(){
+    if($scope.readText === "Read More"){
+      $scope.readText = "Hide";
+      return $scope.readText;
     }
+    else{
+      $scope.readText = "Read More";
+      return $scope.readText;
+    }
+}
 
-    })();
-    });
+readMoreController.$inject = ['$scope'];
+});
