43fb47fa8bf172530113bbd14f5fb8088e3e7322
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2021-02-06 20:41:19 +0000

    Add files via upload

---
+++ books/script.js
@@ -0,0 +1,30 @@
+$.noConflict();
+jQuery(document).ready(function($){
+
+  //Search
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+  //Animation text (not in mobile)
+  $("h2").hover(function(){
+    $("h2").animate({
+      left: '45%',
+      fontSize: "5em"
+    },"slow");
+  });
+
+  //Show books
+  $("#books2021").click(function(){
+    $("#languageList2021").fadeToggle(1000);
+  });
+
+  $("#books2020").click(function(){
+    $("#languageList2020").fadeToggle(1000);
+  });
+  
+});
+
